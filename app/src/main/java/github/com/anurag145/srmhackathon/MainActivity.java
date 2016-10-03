463b2692f83065c8a4da7c104d3bdb9e547848c5
anurag145 <anurag145@gmail.com>
2016-10-03 21:32:53 +0530

    method to fetch data created

--- app/src/main/java/github/com/anurag145/srmhackathon/MainActivity.java
+++ app/src/main/java/github/com/anurag145/srmhackathon/MainActivity.java
@@ -1,15 +1,18 @@
 package github.com.anurag145.srmhackathon;
 
-import android.app.Dialog;
+
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.os.AsyncTask;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.TextView;
 import com.estimote.sdk.SystemRequirementsChecker;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Arrays;
 import github.com.anurag145.srmhackathon.estimote.BeaconID;
 import github.com.anurag145.srmhackathon.estimote.EstimoteCloudBeaconDetails;
@@ -42,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
                     EstimoteCloudBeaconDetails beaconDetails = (EstimoteCloudBeaconDetails) content;
 
                           textView.setText("FOUND 1 "+beaconDetails.getBeaconName());
+                          getjson("",getApplicationContext());
 
                 } else {
                     textView.setText("Kya dekh raha hai bhai niche scroll kar");
@@ -75,10 +79,32 @@ public class MainActivity extends AppCompatActivity {
             }
 
             @Override
-            protected String doInBackground(String... strings) {
-                return null;
+            protected String doInBackground(String... params) {
+                String uri=params[0];
+                BufferedReader bufferedReader = null;
+                try {
+                    java.net.URL url = new URL(uri);
+                    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                    StringBuilder sb = new StringBuilder();
+
+                    bufferedReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+                    String json;
+                    while ((json = bufferedReader.readLine()) != null) {
+                        sb.append(json + "\n");
+                    }
+
+                    return sb.toString().trim();
+
+                } catch (Exception e) {
+                    return "hello";
+                }
+
+
             }
         }
+        GETJSON ob = new GETJSON();
+        ob.execute(URL);
     }
     @Override
     protected void onResume() {
